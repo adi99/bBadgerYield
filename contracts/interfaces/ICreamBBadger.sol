337d055886fb4fb3f734d34cb5305f6c857b230b
Aditya Gautam <adityagautam18@gmail.com>
2021-05-24 01:24:31 +0530

    Create ICreamBBadger.sol

---
+++ contracts/interfaces/ICreamBBadger.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+interface ICreamBBadger {
+  function mint(uint mintAmount) external returns (uint);
+  function redeem(uint redeemTokens) external returns (uint);
+  function redeemUnderlying(uint redeemAmount) external returns (uint);
+  function borrow(uint borrowAmount) external returns (uint);
+  function repayBorrow(uint repayAmount) external returns (uint);
+  function repayBorrowBehalf(address borrower, uint repayAmount) external returns (uint);
+  function getCash() external view returns (uint);
+  function reserveFactorMantissa() external view returns (uint);
+  function totalBorrows() external view returns (uint);
+  function totalReserves() external view returns (uint);
+  function underlying() external view returns (uint);
+  function exchangeRateStored() external view returns (uint);
+}
